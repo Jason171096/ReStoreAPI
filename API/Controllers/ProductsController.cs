0d5972980be80a80a805eb0c19c3b8797b096146
Jason171096 <bboyace10@gmail.com>
2024-05-17 14:23:28 -0600

    First commit

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -29,6 +29,6 @@ namespace API.Controllers
         public async Task<ActionResult<Product>> GetProduct(int id) 
         {
             return await _context.Products.Find(id);
-        }
+        } 
     }
 }
